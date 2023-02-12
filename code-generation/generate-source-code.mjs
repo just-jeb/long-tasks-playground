56bcc8bac1a5b72efe1a4b5bdba3edf021a69a7d
JeB <evgenyba@wix.com>
2023-02-12 15:33:22 +0200

    add single large script

--- code-generation/generate-source-code.mjs
+++ code-generation/generate-source-code.mjs
@@ -26,9 +26,25 @@ const generateScripts = (fileIndex, skipImport = false) => {
     }
 }
 
+const generateSingleLargeScript = () => {
+    const functionsAmount = 100000;
+    let content = '';
+    // Generate a lot of functions
+    for(let i = 0; i < functionsAmount; i++) {
+        const func = `function func${i}() { console.log('func${i}'); }\n`;
+        content += func;
+    }
+    // Enclose the functions in a function to avoid global declarations
+    content = `function delayedFunctions() { \n${content}\n}`
+    // Add a function that is invoked (unlike the previous)
+    content = `invokedFunction();\nfunction invokedFunction() { console.log('invokedFunction'); }\n${content}`
+    fs.writeFileSync(`src/generated-single-file/index.js`, content);
+}
+
 for (let i = 0; i < numberOfFiles; i++) {
     generateRandomJsDataFile({fileName: `src/generated-data/content${i}.js`});
     generateRandomJsDataFile({fileName: `src/generated-data/content-fn${i}.js`, functionize: true});
     generateScripts(i, i == numberOfFiles - 1);
+    generateSingleLargeScript();
 }
 
